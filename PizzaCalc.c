15c2e3cecda7c57dd5a571700e7fea632ed353f9
Jincheng He <jh622@duke.edu>
2021-09-12 04:06:49 -0400

    Update PizzaCalc with working solutions to small files but still have problems for a big input file

--- PizzaCalc.c
+++ PizzaCalc.c
@@ -11,7 +11,7 @@ int isDone(char *str, int len)
     {
         return 1;
     }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 5; i++)
     {
         if (str[i] != done_str[i])
         {
@@ -23,14 +23,15 @@ int isDone(char *str, int len)
 
 struct Pizza
 {
-    char name[60];
+    char name[64];
     float diameter;
     float cost;
+    float pizza_per_dolloar;
 };
 
 void copy_name(char *copied_in, char *copy_origin)
 {
-    int len = strlen(copy_origin) - 1;
+    int len = strlen(copy_origin);
     for (int i = 0; i < len; i++)
     {
         copied_in[i] = copy_origin[i];
@@ -38,30 +39,62 @@ void copy_name(char *copied_in, char *copy_origin)
     copied_in[len] = 0;
 }
 
+void sortPizzaList(struct Pizza *pizza_list, int pizza_count)
+{
+    // Start sorting the pizza list
+    int maxPPD;
+    int maxIndex;
+
+    int tempInt;
+    float tempFloat;
+    for (int i = 0; i < pizza_count - 1; i++)
+    {
+        maxPPD = pizza_list[i].pizza_per_dolloar;
+        maxIndex = i;
+
+        for (int j = i; j < pizza_count; j++)
+        {
+            if (pizza_list[j].pizza_per_dolloar > maxPPD)
+            {
+                maxPPD = pizza_list[i].pizza_per_dolloar;
+                maxIndex = j;
+            }
+        }
+        struct Pizza temp_pointer = pizza_list[i];
+        pizza_list[i] = pizza_list[maxIndex];
+        pizza_list[maxIndex] = temp_pointer;
+    }
+}
+
+int pizza_count = 0;
+
 int main(int argc, char *argv[])
 {
+    // Check input parameters
+    if (argc == 1)
+    {
+        printf("PIZZA FILE IS EMPTY\n");
+    }
+
     if (argc == 2)
     {
         FILE *input_file = fopen(argv[1], "r");
-        char arr[60];
+        char arr[64];
 
         int pizza_list_length = 10;
-        int pizza_count = 0;
         struct Pizza *pizza_list = (struct Pizza *)malloc(sizeof(struct Pizza) * pizza_list_length);
 
+        // Read data into memory
         while (1)
         {
-            if (fgets(arr, 60, input_file) != NULL)
+            if (fscanf(input_file, "%s", arr) != EOF)
             {
-                printf("current line = %s\n", arr);
-                if (!isDone(arr, 60))
+                if (!isDone(arr, strlen(arr)))
                 {
-                    printf("current pizza count = %d\n", pizza_count);
                     // Determine whether the size of pizza list is enough
                     if (pizza_count >= pizza_list_length)
                     {
-                        printf("size = %d\n", pizza_count);
-                        struct Pizza *pizzalist = (struct Pizza *)realloc(pizza_list, 2 * pizza_list_length * sizeof(struct Pizza));
+                        pizza_list = (struct Pizza *)realloc(pizza_list, 2 * pizza_list_length * sizeof(struct Pizza));
                         pizza_list_length = 2 * pizza_list_length;
                     }
 
@@ -69,12 +102,12 @@ int main(int argc, char *argv[])
                     copy_name(pizza_list[pizza_count].name, arr);
 
                     // Put into diameter value
-                    fgets(arr, 60, input_file);
+                    fscanf(input_file, "%s", arr);
                     float diameter = atof(arr);
                     pizza_list[pizza_count].diameter = diameter;
 
                     // Put into cost value
-                    fgets(arr, 60, input_file);
+                    fscanf(input_file, "%s", arr);
                     float cost = atof(arr);
                     pizza_list[pizza_count].cost = cost;
                     pizza_count++;
@@ -87,16 +120,33 @@ int main(int argc, char *argv[])
         }
         fclose(input_file);
 
+        // Begin processing the calculation
         for (int i = 0; i < pizza_count; i++)
         {
-            printf("The name is: %s\n", pizza_list[i].name);
-            printf("The diameter is: %f\n", pizza_list[i].diameter);
-            printf("The cost is: %f\n", pizza_list[i].cost);
+            if (pizza_list[i].cost == 0.0)
+            {
+                pizza_list[i].pizza_per_dolloar = 0;
+            }
+            else
+            {
+                pizza_list[i].pizza_per_dolloar = PI * pizza_list[i].diameter * pizza_list[i].diameter / 4 / pizza_list[i].cost;
+            }
         }
-    }
-    if (argc == 1)
-    {
-        printf("PIZZA FILE IS EMPTY\n");
+
+        sortPizzaList(pizza_list, pizza_count);
+
+        // Show the original information
+        // printf("All Pizza Count = %d\n", pizza_count);
+        for (int i = 0; i < pizza_count; i++)
+        {
+            // printf("The name is: %s\n", pizza_list[i].name);
+            // printf("The diameter is: %f\n", pizza_list[i].diameter);
+            // printf("The cost is: %f\n", pizza_list[i].cost);
+            // printf("The pizza_per_dollar is: %f\n", pizza_list[i].pizza_per_dolloar);
+            printf("%s %f\n", pizza_list[i].name, pizza_list[i].pizza_per_dolloar);
+        }
+
+        free(pizza_list);
     }
     return EXIT_SUCCESS;
 }
